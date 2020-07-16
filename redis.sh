0fa06aa9ba804b35ef4fcde550116362ff5127ff
wsjsx666 <46876785+wsjsx666@users.noreply.github.com>
2020-07-16 14:54:38 +0800

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
-for i in {51..56}
+for i in {2,10}
 do
-scp ~/08.dba1/soft/redis/redis-4.0.8.tar.gz 192.168.4.$i:/root
-ssh 192.168.4.$i \
+scp ~/redis-5.0.9.tar.gz 192.168.1.$i:/root/
+ssh 192.168.1.$i \
 "yum -y install gcc gcc-c++ make expect
-tar -xf redis-4.0.8.tar.gz 
-cd redis-4.0.8/
+tar -xf /root/redis-5.0.9.tar.gz
+cd /root/redis-5.0.9
 make && make install
 cd utils/
-expect<<EOF
+expect <<EOF
 spawn ./install_server.sh
 expect ""6379]"" {send ""\r""}
 expect ""conf]"" {send ""\r""}
-expect "".log]""  {send ""\r""}
-expect ""/6379]"" {send ""\r""}
-expect ""server]"" {send ""\r""}
+expect "".log""  {send ""\r""}
+expect ""/6379"" {send ""\r""}
+expect ""server"" {send ""\r""}
 expect ""abort."" {send ""\r""}
-expect ""#"" {send "exit"\r""}
+expect ""#""     {send ""exit\r""}
 EOF
-ss -ntulp | grep :6379
-[ $? -eq 0 ]&& echo 'redis启动成功' ||echo '搭建失败,请调试!'
-sed -i '/port 6379/c "port 63$i"' /etc/redis/6379.conf
-sed -i '/bind/s/127.0.0.1/192.168.4.${i}/' /etc/redis/6379.conf
-sed -i '/pidfile \/var/run/redis_6379.pid/s/6379/63${i}/' /etc/redis/6379.conf
-sed -i '/cluster-enabled/s/\#//' /etc/redis/6379.conf
+netstat -ntlp|grep :6379
+[ $? -eq 0 ] && echo 'redis,SUCCESS' || echo 'ERROR,Plz check'
+sed -i '/^port 6379/c "port 63$i"' /etc/redis/6379.conf
+sed -i '/^bind/s/127.0.0.1/192.168.1.${i}/' /etc/redis/6379.conf
+sed -i '/pidfile \/var\/run\/redis_6379.pid/s/6379/63${i}/' /etc/redis/6379.conf
+sed -i '/cluster-enabled/s/#//' /etc/redis/6379.conf
 sed -i '/cluster-config-file/c cluster-config-file nodes-63${i}.conf' /etc/redis/6379.conf
 sed -i '/cluster-node-timeout/c cluster-node-timeout 5000' /etc/redis/6379.conf
-/etc/init.d/redis_6379 start
-ss -antlp | grep 63$i
+mv /etc/redis/6379.conf /etc/redis/63${i}.conf
+sed -i 's/6379/63${i}/g' /etc/init.d/redis_6379
+mv /etc/init.d/redis_6379 /etc/init.d/redis_63${i}
+/etc/init.d/redis_63${i} start
+netstat -ntlp|grep 63${i}
 sleep 2
 "
 done
-
